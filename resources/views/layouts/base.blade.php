1e4483aa615e698313d65015baa5662a076a9c6e
Alex Esaki <apachecordovax@gmail.com>
2020-02-14 22:43:09 +0100

    stylisation: ajout du fichier app.css

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,36 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Url Shortener</title>
-    <style>
-        .error-msg {
-            color: pink;
-            font-style: italic;
-            font-weight: bold;
-        }
-
-        body {
-            background : #777;
-            color: #fff;
-        }
-
-        .wrapper {
-            width: 680px;
-            margin: 0 auto;
-            padding-top: 8px;
-            text-align: center; 
-        }
-
-        input[type='text'] {
-            width: 100%;
-            border: none;
-            padding: 1em 0.5em;
-            border-radius: 3px;
-        }
-
-        a {
-            color: #ccc;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
 </head>
 <body>
     <div class="wrapper">
